e3802897f3b4121fb6943d8a41440eee957f1db6
ChrisL <cladden@users.noreply.github.com>
2022-12-01 10:25:17 -0500

    Allow node selection by path using an env var
    The environment variable `DPE_NODE_PATH_FILTER="some/path/filter"` can be set to only evaluate specific nodes where the node's file path contains this string value.

--- macros/unpack/get_nodes.sql
+++ macros/unpack/get_nodes.sql
@@ -5,32 +5,34 @@
 {%- macro default__get_nodes() -%}
 
     {%- if execute -%}
+    {%- set node_path_filter = env_var('DPE_NODE_PATH_FILTER', None) -%}
     {%- set nodes_list = graph.nodes.values() -%}
     {%- set values = [] -%}
 
     {%- for node in nodes_list -%}
+        {%- if node_path_filter is None or node_path_filter in node.path -%}
+            {{ log('Evaluating node: ' ~ node.name, info=True) }}
+            {%- set values_line  =
+                [
+                    wrap_string_with_quotes(node.unique_id),
+                    wrap_string_with_quotes(node.name),
+                    wrap_string_with_quotes(node.resource_type),
+                    wrap_string_with_quotes(node.original_file_path | replace("\\","\\\\")),
+                    "cast(" ~ node.config.enabled | trim ~ " as boolean)",
+                    wrap_string_with_quotes(node.config.materialized),
+                    wrap_string_with_quotes(node.config.on_schema_change),
+                    wrap_string_with_quotes(node.database),
+                    wrap_string_with_quotes(node.schema),
+                    wrap_string_with_quotes(node.package_name),
+                    wrap_string_with_quotes(node.alias),
+                    "cast(" ~ dbt_project_evaluator.is_not_empty_string(node.description) | trim ~ " as boolean)",
+                    "''" if not node.column_name else wrap_string_with_quotes(dbt.escape_single_quotes(node.column_name)),
+                    wrap_string_with_quotes(node.meta | tojson)
+                ]
+            %}
 
-        {%- set values_line  = 
-            [
-                wrap_string_with_quotes(node.unique_id),
-                wrap_string_with_quotes(node.name),
-                wrap_string_with_quotes(node.resource_type),
-                wrap_string_with_quotes(node.original_file_path | replace("\\","\\\\")),
-                "cast(" ~ node.config.enabled | trim ~ " as boolean)",
-                wrap_string_with_quotes(node.config.materialized),
-                wrap_string_with_quotes(node.config.on_schema_change),
-                wrap_string_with_quotes(node.database),
-                wrap_string_with_quotes(node.schema),
-                wrap_string_with_quotes(node.package_name),
-                wrap_string_with_quotes(node.alias),
-                "cast(" ~ dbt_project_evaluator.is_not_empty_string(node.description) | trim ~ " as boolean)",
-                "''" if not node.column_name else wrap_string_with_quotes(dbt.escape_single_quotes(node.column_name)),
-                wrap_string_with_quotes(node.meta | tojson)
-            ]
-        %}
-
-        {%- do values.append(values_line) -%}
-
+            {%- do values.append(values_line) -%}
+        {%- endif -%}
     {%- endfor -%}
     {%- endif -%}
 
